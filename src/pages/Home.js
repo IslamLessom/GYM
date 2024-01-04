a529ad0330c05f22936eb37fca955fc6f686ce33
islam <mem.m.1979@mail.ru>
2024-01-04 14:15:53 +0300

    add

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
+};
