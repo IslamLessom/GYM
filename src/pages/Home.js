c905bf4a706141c8bf36f61de3474eb137fcf681
islam <mem.m.1979@mail.ru>
2024-01-05 16:38:16 +0300

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 import React from "react";
-
+import HeroBanner from "../components/HeroBanner";
+import  SearchExercises  from "../components/SearchExercises";
+import { Excersices } from "../components/Excersices";
 export const Home = () => {
   return (
     <Box>
       <HeroBanner />
       <SearchExercises />
-      <Exercises />
+      <Excersices />
     </Box>
   );
 };
